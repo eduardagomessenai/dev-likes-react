66ca675bd7d1322399505bc63ca0197bf0fc576b
dudsssenai <eduardagomesdesouza8@gmail.com>
2023-08-11 14:54:52 -0300

    socialLinks desenvolvido

---
+++ src/components/socialLinks/socialLinks.jsx
@@ -0,0 +1,10 @@
+import "./socialLinks.module.css";
+
+const SocialLinks = ({ icon, href }) => {
+  return (
+    <a href={href} target="_blank">
+      <ion-icon name={icon} />
+    </a>
+  );
+};
+export default SocialLinks;
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,8 @@
-
 import { useState } from "react";
 import Profile from "../components/profile/profile";
 import Switch from "../components/switch/switch";
 import Links from "../components/links/links";
+import SocialLinks from "../components/socialLinks/socialLinks";
 
 import "./App.css";
 
@@ -10,25 +10,31 @@ import imgDarkMode from "../img/eudnv.jpg";
 import imgLightMode from "../img/126248825.jpg";
 
 const App = () => {
-const [isLightMode,setIsLightMode] = useState(true)
+  const [isLightMode, setIsLightMode] = useState(true);
 
-const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
-const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro"; 
-return (
+  const imgSrc = isLightMode ? imgLightMode : imgDarkMode;
+  const imgAlt = isLightMode ? "Imagem no Modo Claro" : "Imagem no Modo Escuro";
+  return (
     <div id="container">
-      <Profile imgSrc={imgSrc} imgAlt={imgAlt}>@gomes_eduarda__</Profile>
+      <Profile imgSrc={imgSrc} imgAlt={imgAlt}>
+        @gomes_eduarda__
+      </Profile>
       <Switch></Switch>
-    <div id="links">
-      <ul> 
-      <Links link={"https://github.com"}>GitHub</Links>
-      <Links link={"#"}>Instagram</Links>
-      <Links link={"#"}>Portifólio</Links>
-      <Links link={"#"}>Projetos</Links>
-      </ul>
-
+      <div id="links">
+        <ul>
+          <Links link={"https://github.com"}>GitHub</Links>
+          <Links link={"#"}>Instagram</Links>
+          <Links link={"#"}>Portifólio</Links>
+          <Links link={"#"}>Projetos</Links>
+        </ul>
+      </div>
+      <div id="socialLinks">
+        <SocialLinks href={"#"} icon={"logo-github"} />
+        <SocialLinks href={"#"} icon={"logo-instagram"} />
+        <SocialLinks href={"#"} icon={"logo-facebook"} />
+        <SocialLinks href={"#"} icon={"logo-twitter"} />
       </div>
     </div>
-
-);
+  );
 };
 export default App;
